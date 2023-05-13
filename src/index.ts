b5e92b787b9896ceb0ade530b2ec137640c9670c
Thomas Donohue <donth77@gmail.com>
2023-05-13 19:12:01 -0400

    Set up prod to run

--- src/index.ts
+++ src/index.ts
@@ -1,12 +1,17 @@
 import express from 'express';
 import routes from './routes';
 const app = express();
-const port = 3000;
+const port = process.env.PORT || 3000;
 
 app.use('/api', routes);
 
+app.all('*', (_, res, next) => {
+  res.set('Access-Control-Allow-Origin', '*');
+  next();
+});
+
 app.listen(port, () => {
-  console.log(`Server started at port ${port}`);
+  console.log(`Server running on port ${port}`);
 });
 
 export default app;
